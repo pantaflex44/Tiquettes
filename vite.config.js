ebfbf3e83ffced78d3331e9589713a26e89edf8d
pantaflex44 <pantaflex@tuta.io>
2024-05-26 22:08:33 +0200

    Bug Fix. More SEO Friendly

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 import ogPlugin from 'vite-plugin-open-graph';
 
 // https://vitejs.dev/config/
@@ -19,42 +18,30 @@ export default ({ mode }) => {
     options = {
       ...options, plugins: [
         ...options.plugins,
-        ViteFaviconsPlugin({
-          logo: './public/favicon.svg',
-          favicons: {
-            appName: env.VITE_APP_NAME,
-            appDescription: env.VITE_APP_DESCRIPTION,
-            developerName: env.VITE_APP_AUTHOR,
-            developerURL: env.VITE_APP_SOURCES,
-            background: '#009E4D',
-            theme_color: '#333',
-            icons: {
-              coast: false,
-              yandex: false
-            }
+        
+        ogPlugin(
+          {
+            basic: {
+              url: env.VITE_APP_URL,
+              title: env.VITE_APP_NAME,
+              type: 'website',
+              image: `${env.VITE_APP_URL}og.webp`,
+              determiner: 'auto',
+              description: env.VITE_APP_DESCRIPTION,
+              locale: 'fr_FR',
+              localeAlternate: ['fr_FR'],
+              siteName: env.VITE_APP_NAME,
+            },
+            twitter: {
+              image: `${env.VITE_APP_URL}twitter.webp`,
+              card: 'summary_large_image',
+              description: env.VITE_APP_DESCRIPTION,
+              title: env.VITE_APP_NAME,
+              site: env.VITE_APP_URL,
+              creator: env.VITE_APP_AUTHOR,
+            },
           }
-        }),
-        ogPlugin({
-          basic: {
-            url: env.VITE_APP_URL,
-            title: env.VITE_APP_NAME,
-            type: 'website',
-            image: `${env.VITE_APP_URL}og.webp`,
-            determiner: 'auto',
-            description: env.VITE_APP_DESCRIPTION,
-            locale: 'fr_FR',
-            localeAlternate: ['fr_FR'],
-            siteName: env.VITE_APP_NAME,
-          },
-          twitter: {
-            image: `${env.VITE_APP_URL}twitter.webp`,
-            card: 'summary_large_image',
-            description: env.VITE_APP_DESCRIPTION,
-            title: env.VITE_APP_NAME,
-            site: env.VITE_APP_URL,
-            creator: env.VITE_APP_AUTHOR,
-          },
-        })
+        )
       ]
     };
   }
