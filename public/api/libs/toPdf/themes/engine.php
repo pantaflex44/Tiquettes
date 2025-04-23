351009eac6a244ca2de66c1556648e19699adfaf
pantaflex44 <pantaflex@hotmail.fr>
2025-04-23 23:24:34 +0200

    Add and update icons by @cruzfabien

--- public/api/libs/toPdf/themes/engine.php
+++ public/api/libs/toPdf/themes/engine.php
@@ -22,11 +22,30 @@ class Theme
         }
     }
 
+    private static function setFgColor($pdf, $data)
+    {
+        if (array_key_exists('color', $data)
+            && preg_match('/^#[a-f0-9]{6}$/i', $data['color'])
+            && strlen($data['color']) === 7
+        ) {
+            $color = self::computeColor($data['color']);
+            $pdf->SetTextColor($color[0], $color[1], $color[2]);
+        } else {
+            $pdf->SetTextColor(0, 0, 0);
+        }
+    }
+
     private static function renderIcon($pdf, $workBox, $data, $module, $count)
     {
-        $sizeMm = array_key_exists('sizePercent', $data) ? ((50 + ($data['sizePercent'] / 2)) / 100) * 10 : 10;
+
 
         self::setBgColor($pdf, $data);
+    }
+
+    private static function renderText($pdf, $workBox, $data, $module, $count)
+    {
+
+
     }
 
     public static function render($pdf, $workBox, $themeData, $module)
@@ -37,24 +56,64 @@ class Theme
             fn($val, $key) => array_key_exists('shown', $val) && $val['shown'] === true,
             ARRAY_FILTER_USE_BOTH
         );
-        if (count($data) === 0) return;
+        $count = count(array_keys($data));
+        if ($count === 0) return;
 
         $data = array_map(fn($k) => array_merge($k, [
             'position' => $k['position'] === 'top' ? 0 : ($k['position'] === 'middle' ? 1 : 2),
         ]), $data);
-        $count = count(array_keys($data));
-        $data = array_map(fn($k) => array_merge($k, [
-            'margins' => [
-                'top' => $k['position'] === 0 ? 2 : 1,
-                'bottom' => $k['position'] === $count - 1 ? 2 : 1,
+        array_multisort(array_column($data, 'position'), SORT_ASC, $data);
+        $pos = 0;
+        $np = $workBox['y'];
+        foreach (array_keys($data) as $key) {
+            $data[$key]['margins'] = [
+                'top' => $pos === 0 ? 2 : 1,
+                'bottom' => $pos === $count - 1 ? 2 : 1,
                 'left' => 1,
                 'right' => 1,
-            ],
-        ]), $data);
-        array_multisort(array_column($data, 'position'), SORT_ASC, $data);
+            ];
+            $data[$key]['position'] = $pos++;
 
-        foreach ($data as $key => $val) {
-            if ($key === 'icon') self::renderIcon($pdf, $workBox, $val, $module, $count);
+            self::setBgColor($pdf, $data);
+
+            if ($key === 'id' || $key === 'text') {
+                self::setFgColor($pdf, $data);
+
+                $data[$key]['fontFamily'] = strtolower(trim($data[$key]['fontFamily'] ?? 'sans-serif'));
+                if ($data[$key]['fontFamily'] === 'serif') $data[$key]['fontFamily'] = 'Times';
+                else if ($data[$key]['fontFamily'] === 'monospace') $data[$key]['fontFamily'] = 'Courier';
+                else if ($data[$key]['fontFamily'] === 'cursive') $data[$key]['fontFamily'] = 'Symbol';
+                else $data[$key]['fontFamily'] = 'Helvetica';
+                $data[$key]['fontSize'] = (float)($data[$key]['fontSize'] ?? 2.4);
+                $data[$key]['fontSizePt'] = $data[$key]['fontSize'] / 0.35277777777778; // mm to pt
+                $data[$key]['fontStyle'] = strtolower(trim($data[$key]['fontStyle'] ?? 'normal'));
+                $data[$key]['fontWeight'] = strtolower(trim($data[$key]['fontWeight'] ?? 'normal'));
+                $s = $data[$key]['fontStyle'] === 'italic' ? 'I' : '';
+                $s .= $data[$key]['fontWeight'] === 'bold' ? 'B' : '';
+                $data[$key]['fontStyle'] = $s;
+                $data[$key]['fontWeight'] = '';
+                $pdf->SetFont($data[$key]['fontFamily'], $data[$key]['fontStyle'], $data[$key]['fontSizePt']);
+
+                $data[$key]['lineCount'] = (int)($data[$key]['lineCount'] ?? 1);
+                $data[$key]['place'] = [
+                    'w' => $workBox['w'] - $data[$key]['margins']['left'] - $data[$key]['margins']['right'],
+                    'h' => ($data[$key]['lineCount'] * $data[$key]['fontSize']) + (($data[$key]['lineCount'] - 1) * 0.5373),
+                ];
+            }
+            if ($key === 'icon') {
+                $sizeMm = array_key_exists('sizePercent', $data[$key]) ? ((50 + ($data[$key]['sizePercent'] / 2)) / 100) * 10 : 10;
+                $data[$key]['place'] = [
+                    'w' => $sizeMm,
+                    'h' => $sizeMm,
+                ];
+            }
+            $data[$key]['bgPlace'] = [
+                'x' => $workBox['x'],
+                'y' => $np,
+                'w' => $workBox['w'],
+                'h' => $data[$key]['place']['h'] + $data[$key]['margins']['top'] + $data[$key]['margins']['bottom'],
+            ];
+            $np = $data[$key]['bgPlace']['y'] + $data[$key]['bgPlace']['h'];
         }
 
         var_dump($data);
