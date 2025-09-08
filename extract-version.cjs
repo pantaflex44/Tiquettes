be8ed0b825240353bfd84ca97548e48c57fd1e3f
pantaflex44 <pantaflex@tuta.io>
2025-09-08 15:35:07 +0200

    Update package.json properties extractor

--- extract-version.cjs
+++ extract-version.cjs
@@ -1,8 +1,17 @@
+console.log("Package.json properties extractor ...");
+
 const pkg = require("./package.json");
+
+let now = new Date(Date.now());
+const offset = now.getTimezoneOffset()
+now = new Date(now.getTime() - (offset * 60 * 1000))
+now = now.toISOString().split('T')[0]
+
 const infos = JSON.stringify({
-    name: pkg.title,
-    version: pkg.version,
-    description: pkg.description
+    name: pkg.title ?? "",
+    version: pkg.version ?? "",
+    description: pkg.description ?? "",
+    date: !pkg.date ? now : `${pkg.date.year}-${String(pkg.date.month).padStart(2, '0')}-${String(pkg.date.day).padStart(2, '0')}`,
 });
 
 const fs = require("fs");
