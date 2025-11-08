0981973a6a4da31dba96a2a4568975598c36e200
pantaflex44 <pantaflex@tuta.io>
2025-11-08 08:17:55 +0100

    Detect new version and ask to reload before using it

--- public/api/libs/stats.sql
+++ public/api/libs/stats.sql
@@ -1,135 +1,275 @@
--- Adminer 5.4.1 MariaDB 10.11.13-MariaDB-0ubuntu0.24.04.1 dump
+-- phpMyAdmin SQL Dump
+-- version 4.9.11
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db5018814848.hosting-data.io
+-- Généré le : sam. 08 nov. 2025 à 05:50
+-- Version du serveur : 10.11.14-MariaDB-log
+-- Version de PHP : 7.4.33
 
-SET NAMES utf8;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET time_zone = '+00:00';
 
-SET foreign_key_checks = 0;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+--
+-- Base de données : `dbs14862378`
+--
+CREATE DATABASE IF NOT EXISTS `dbs14862378` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
+USE `dbs14862378`;
 
-SET NAMES utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_action_create`
+--
 
 DROP TABLE IF EXISTS `stats_action_create`;
-
 CREATE TABLE `stats_action_create` (
-    `date` date NOT NULL DEFAULT current_timestamp(),
-    `struct` varchar(10) NOT NULL,
-    `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`)),
-    PRIMARY KEY (`date`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `struct` varchar(10) NOT NULL,
+  `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_action_export`
+--
 
 DROP TABLE IF EXISTS `stats_action_export`;
-
 CREATE TABLE `stats_action_export` (
-    `date` date NOT NULL DEFAULT current_timestamp(),
-    `struct` varchar(10) NOT NULL,
-    `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`)),
-    PRIMARY KEY (`date`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `struct` varchar(10) NOT NULL,
+  `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_action_import`
+--
 
 DROP TABLE IF EXISTS `stats_action_import`;
-
 CREATE TABLE `stats_action_import` (
-    `date` date NOT NULL DEFAULT current_timestamp(),
-    `struct` varchar(10) NOT NULL,
-    `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`)),
-    PRIMARY KEY (`date`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `struct` varchar(10) NOT NULL,
+  `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_action_print`
+--
 
 DROP TABLE IF EXISTS `stats_action_print`;
-
 CREATE TABLE `stats_action_print` (
-    `date` date NOT NULL DEFAULT current_timestamp(),
-    `struct` varchar(10) NOT NULL,
-    `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`)),
-    PRIMARY KEY (`date`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `struct` varchar(10) NOT NULL,
+  `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`counters`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_allowed_actions`
+--
 
 DROP TABLE IF EXISTS `stats_allowed_actions`;
-
 CREATE TABLE `stats_allowed_actions` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `key` varchar(10) NOT NULL,
-    `description` text NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `id` int(11) NOT NULL,
+  `key` varchar(10) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-TRUNCATE `stats_allowed_actions`;
+--
+-- Déchargement des données de la table `stats_allowed_actions`
+--
 
-INSERT INTO
-    `stats_allowed_actions` (`id`, `key`, `description`)
-VALUES (
-        1,
-        'create',
-        'Création d\'un nouveau projet'
-    ),
-    (
-        2,
-        'print',
-        'Impression d\'un projet'
-    ),
-    (
-        3,
-        'import',
-        'Importation d\'un projet'
-    ),
-    (
-        4,
-        'export',
-        'Exportation d\'un projet'
-    );
+INSERT INTO `stats_allowed_actions` (`id`, `key`, `description`) VALUES
+(1, 'create', 'Création d\'un nouveau projet'),
+(2, 'print', 'Impression d\'un projet'),
+(3, 'import', 'Importation d\'un projet'),
+(4, 'export', 'Exportation d\'un projet');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_allowed_choices`
+--
+
+DROP TABLE IF EXISTS `stats_allowed_choices`;
+CREATE TABLE `stats_allowed_choices` (
+  `id` int(11) NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `stats_allowed_choices`
+--
+
+INSERT INTO `stats_allowed_choices` (`id`, `key`, `description`) VALUES
+(1, 'theme', 'Nom du thème utilisé'),
+(7, 'uses', 'Utilisations');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_allowed_structs`
+--
 
 DROP TABLE IF EXISTS `stats_allowed_structs`;
-
 CREATE TABLE `stats_allowed_structs` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `key` varchar(10) NOT NULL,
-    `description` text NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `id` int(11) NOT NULL,
+  `key` varchar(10) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-TRUNCATE `stats_allowed_structs`;
+--
+-- Déchargement des données de la table `stats_allowed_structs`
+--
 
-INSERT INTO
-    `stats_allowed_structs` (`id`, `key`, `description`)
-VALUES (
-        1,
-        'app',
-        'Application Tiquettes.fr'
-    ),
-    (
-        2,
-        'web',
-        'Site web de l\'application'
-    );
+INSERT INTO `stats_allowed_structs` (`id`, `key`, `description`) VALUES
+(1, 'app', 'Application Tiquettes.fr'),
+(2, 'web', 'Site web de l\'application');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_visits`
+--
+
+DROP TABLE IF EXISTS `stats_visits`;
+CREATE TABLE `stats_visits` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `regionName` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `timezone` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `struct` varchar(10) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `ua` text NOT NULL,
+  `rfr` text NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats_visits_details`
+--
 
 DROP TABLE IF EXISTS `stats_visits_details`;
-DROP TABLE IF EXISTS `stats_visits`;
-
-CREATE TABLE `stats_visits` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `ip` varchar(255) NOT NULL,
-    `country` varchar(255) NOT NULL,
-    `regionName` varchar(255) NOT NULL,
-    `city` varchar(255) NOT NULL,
-    `timezone` varchar(255) NOT NULL,
-    `type` varchar(15) NOT NULL,
-    `struct` varchar(15) NOT NULL,
-    `url` varchar(255) NOT NULL,
-    `ua` text NOT NULL,
-    `rfr` text NOT NULL,
-    `datetime` datetime NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (`id`),
-    KEY `date` (`datetime`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-
 CREATE TABLE `stats_visits_details` (
-    `visit_id` int(11) NOT NULL,
-    `date` date NOT NULL DEFAULT current_timestamp(),
-    `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`counters`)),
-    PRIMARY KEY (`visit_id`),
-    CONSTRAINT `stats_visits_details_ibfk_1` FOREIGN KEY (`visit_id`) REFERENCES `stats_visits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `visit_id` int(11) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `counters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`counters`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 2025-10-20 19:50:59 UTC
+--
+-- Index pour la table `stats_action_create`
+--
+ALTER TABLE `stats_action_create`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Index pour la table `stats_action_export`
+--
+ALTER TABLE `stats_action_export`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Index pour la table `stats_action_import`
+--
+ALTER TABLE `stats_action_import`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Index pour la table `stats_action_print`
+--
+ALTER TABLE `stats_action_print`
+  ADD PRIMARY KEY (`date`);
+
+--
+-- Index pour la table `stats_allowed_actions`
+--
+ALTER TABLE `stats_allowed_actions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `stats_allowed_choices`
+--
+ALTER TABLE `stats_allowed_choices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `stats_allowed_structs`
+--
+ALTER TABLE `stats_allowed_structs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `stats_visits`
+--
+ALTER TABLE `stats_visits`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `date` (`datetime`);
+
+--
+-- Index pour la table `stats_visits_details`
+--
+ALTER TABLE `stats_visits_details`
+  ADD PRIMARY KEY (`visit_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `stats_allowed_actions`
+--
+ALTER TABLE `stats_allowed_actions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT pour la table `stats_allowed_choices`
+--
+ALTER TABLE `stats_allowed_choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT pour la table `stats_allowed_structs`
+--
+ALTER TABLE `stats_allowed_structs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT pour la table `stats_visits`
+--
+ALTER TABLE `stats_visits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `stats_visits_details`
+--
+ALTER TABLE `stats_visits_details`
+  ADD CONSTRAINT `stats_visits_details_ibfk_1` FOREIGN KEY (`visit_id`) REFERENCES `stats_visits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
