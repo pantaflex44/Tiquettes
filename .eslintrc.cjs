5c18335e31cd71e38c3791f673c313a511bd543f
Christophe LEMOINE <pantaflex44@tuta.io>
2026-05-19 22:03:01 +0200

    Mise à jour des modules node.js.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: '19.2.6' } },
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 0,
