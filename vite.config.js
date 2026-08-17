fd3ff39e93275e581c6546ae5b05d6ef5c3fd65f
pantaflex44 <pantaflex@tuta.io>
2026-08-17 09:20:12 +0200

    Modification des Github Actions

--- vite.config.js
+++ vite.config.js
@@ -57,7 +57,14 @@ export default ({ mode }) => {
             rollupOptions: {
                 external: [
                     "sharp"
-                ]
+                ],
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    },
+                },
             }
         }
     };
